3c578b78d360890558bc804bfc19ea784af457c8
JanKrzemien <jan.krzemien.6@gmail.com>
2024-07-23 17:54:05 +0200

    implemented resizing images with settings changed

--- src/main/java/org/example/generator/Controller/ModuleTab.java
+++ src/main/java/org/example/generator/Controller/ModuleTab.java
@@ -3,7 +3,9 @@ package org.example.generator.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.example.generator.config.AppConfig;
@@ -17,11 +19,19 @@ public class ModuleTab extends Controller {
 
     @FXML
     private FlowPane imageListView;
+    @FXML
+    private AnchorPane tileRulesetPane;
 
     @FXML
     public void initialize() {
         displayImages();
         getConfig().getTilesObservableObj().addListener((observed, old_value, new_value) -> displayImages());
+        getConfig().getTile_sizeObj().addListener((observed, old_value, new_value) -> {
+            imageListView.getChildren().forEach((imgView) -> {
+                ((ImageView)imgView).setFitHeight((int)new_value);
+                ((ImageView)imgView).setFitWidth((int)new_value);
+            });
+        });
     }
 
     private void displayImages() {
